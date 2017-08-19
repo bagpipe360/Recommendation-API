73e1ae854e21883f38cba2acc99a2328bafe3f65
bxm6277 <bxm6277@louisiana.edu>
2017-08-19 13:22:05 -0500

    First commit with Swagger being served with WAITR spec Driver Rec spec

---
+++ app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var SwaggerRestify = require('swagger-restify-mw');
+var restify = require('restify');
+var app = restify.createServer();
+
+module.exports = app; // for testing
+
+var config = {
+  appRoot: __dirname // required config
+};
+
+
+// serve swagger json
+app.get(/\/swagger\/?.*/, restify.serveStatic ({
+  directory: './api'
+}));
+
+
+
+app.get(/\/api\/?.*/, restify.serveStatic({
+  directory: './public',
+  default: '.index.html'
+}));
+
+
+SwaggerRestify.create(config, function(err, swaggerRestify) {
+  if (err) { throw err; }
+
+  swaggerRestify.register(app);
+
+  var port = process.env.PORT || 3000;
+  app.listen(port);
+});
